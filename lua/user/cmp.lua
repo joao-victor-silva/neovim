0ed000ef22ae40798cd5a6544e757b0d6498c624
João Victor Rodrigues da Silva <victor.joao.1611@gmail.com>
2022-06-25 00:18:22 -0300

    Fix weird tab behaviour caused by cmp

--- lua/user/cmp.lua
+++ lua/user/cmp.lua
@@ -10,10 +10,15 @@ if not snip_status_ok then
     return
 end
 
-local ctags_status_ok, ctags = pcall(require, "cmp_nvim_tags")
-if not snip_status_ok then
+local ctags_status_ok, _ = pcall(require, "cmp_nvim_tags")
+if not ctags_status_ok then
     vim.notify("ctags not found")
     return
+end
+
+local has_words_before = function()
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 cmp.setup({
@@ -33,16 +38,18 @@ cmp.setup({
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         }),
-        ["<Tab>"] = function(fallback)
+        ["<Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
             else
                 fallback()
             end
-        end,
-        ["<S-Tab>"] = function(fallback)
+        end, { "i", "s" }),
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
             if cmp.visible() then
                 cmp.select_prev_item()
             elseif luasnip.jumpable(-1) then
@@ -50,7 +57,7 @@ cmp.setup({
             else
                 fallback()
             end
-        end,
+        end, { "i", "s" }),
     },
     sources = {
         { name = "nvim_lsp" },
