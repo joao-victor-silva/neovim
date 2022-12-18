26b17cc5ce5b19d35ed395c5919b5c2434c30d62
João Victor Silva <victor.joao.1611@gmail.com>
2022-12-18 02:34:57 -0300

    refactor: remove dead code

--- lua/user/null-ls.lua
+++ lua/user/null-ls.lua
@@ -31,18 +31,6 @@ end
 null_ls.setup({
     debug = false,
     sources = sources,
-    -- sources = {
-    --     null_ls.builtins.diagnostics.luacheck,
-    --     null_ls.builtins.formatting.stylua,
-    --     null_ls.builtins.diagnostics.flake8,
-    --     null_ls.builtins.diagnostics.mypy,
-    --     null_ls.builtins.formatting.blue,
-    --     null_ls.builtins.diagnostics.gitlint.with({ extra_args = { "--contrib=CT1" } }),
-    --     null_ls.builtins.diagnostics.cspell,
-    --     null_ls.builtins.diagnostics.codespell,
-    --     -- null_ls.builtins.diagnostics.misspell,
-    --     null_ls.builtins.formatting.alejandra,
-    -- },
 })
 
 -- Recommendations
